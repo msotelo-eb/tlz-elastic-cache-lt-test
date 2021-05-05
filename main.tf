5000e73d07f64d016d3d099776297cfdf75bb938
msotelo-eb <msotelo@eventbrite.com>
2021-05-05 15:44:06 -0300

    [1] Basic redis deployment

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  backend "remote" {
+    organization = "matisotee"
+
+    workspaces {
+      name = "dev"
+    }
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_elasticache_cluster" "example" {
+  cluster_id           = "cluster-example"
+  engine               = "redis"
+  node_type            = "cache.t2.micro"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis3.2"
+  engine_version       = "3.2.10"
+  port                 = 6379
+}
