baf71e115d55604d8c40ffb820ac3518278bf5b8
msotelo-eb <msotelo@eventbrite.com>
2021-05-12 16:21:44 -0300

    [4] Add variable for desired clusters

--- main.tf
+++ main.tf
@@ -1,10 +1,17 @@
+data "aws_availability_zones" "available" {}
+
+resource "random_shuffle" "availability_zones_list" {
+  input        = data.aws_availability_zones.available.names
+  result_count = var.elasticache_desired_clusters
+}
+
 resource "aws_elasticache_replication_group" "example" {
   automatic_failover_enabled    = true
-  availability_zones            = [join("", [var.aws_region, "a"]), join("", [var.aws_region, "b"])]
+  availability_zones            = random_shuffle.availability_zones_list.result
   replication_group_id          = "tf-rep-group-1"
   replication_group_description = "test description"
   node_type                     = "cache.t2.micro"
-  number_cache_clusters         = 2
+  number_cache_clusters         = var.elasticache_desired_clusters
   engine_version                = "3.2.10"
   parameter_group_name          = "default.redis3.2"
   port                          = 6379
--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
 variable "aws_region" {}
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
+variable "elasticache_desired_clusters" {
+  type = number
+  default = 2
+}
